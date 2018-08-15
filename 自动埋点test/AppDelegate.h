0415af90f0a66de60a6734951c2dd32fc2b86d9d
何志博 <453630969@qq.com>
2018-08-15 14:03:06 +0800

    Initial Commit

---
+++ 自动埋点test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自动埋点test
+//
+//  Created by 何志博 on 2018/8/15.
+//  Copyright © 2018年 何志博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自动埋点test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自动埋点test
+//
+//  Created by 何志博 on 2018/8/15.
+//  Copyright © 2018年 何志博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
